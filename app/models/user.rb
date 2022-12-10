839c19a2189a6d473bce49cfe65994bfee7d2464
ZikaZaki <zika.github@gmail.com>
2022-12-11 01:34:54 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    has_many :likes
+  has_many :posts
+  has_many :comments
+  has_many :likes
 
   def retrieve_recent_posts
     posts.order(:created_at).limit(3)
