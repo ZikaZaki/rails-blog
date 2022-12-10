75896bccb166779638362e95928bb8787ce17129
ZikaZaki <zika.github@gmail.com>
2022-12-10 23:55:44 +0300

    Add post model

---
+++ db/migrate/20221210205503_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :summary
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
