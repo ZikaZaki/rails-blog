45f91d1674c18becf604b1773c2396013df99ef3
ZikaZaki <zika.github@gmail.com>
2022-12-11 00:27:43 +0300

    Add indexes for all tables

--- db/migrate/20221210205503_create_posts.rb
+++ db/migrate/20221210205503_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :content
       t.integer :comments_counter
       t.integer :likes_counter
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, index: true
 
       t.timestamps
     end
