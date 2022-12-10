e222d064f0b424fb40f87c9fdfc566a3834f5d6e
ZikaZaki <zika.github@gmail.com>
2022-12-11 00:14:39 +0300

    Update seeds.rb

--- db/migrate/20221210205503_create_posts.rb
+++ db/migrate/20221210205503_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :summary
+      t.text :content
       t.integer :comments_counter
       t.integer :likes_counter
       t.references :user, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+Like.destroy_all
+
+users = User.create([
+    { name: "user1", photo: "https://i.imgur.com/1ZQZ1Zm.jpg", bio: "I am user1", posts_counter: 0 },
+    { name: "user2", photo: "https://i.imgur.com/1ZQZ1Zm.jpg", bio: "I am user2", posts_counter: 0 },
+    { name: "user3", photo: "https://i.imgur.com/1ZQZ1Zm.jpg", bio: "I am user3", posts_counter: 0 },
+    { name: "user4", photo: "https://i.imgur.com/1ZQZ1Zm.jpg", bio: "I am user4", posts_counter: 0 },
+    { name: "user5", photo: "https://i.imgur.com/1ZQZ1Zm.jpg", bio: "I am user5", posts_counter: 0 }
+])
+
+posts = Post.create([
+    { title: "post1", content: "I am post1", user: users[0], comments_counter: 0, likes_counter: 0 },
+    { title: "post2", content: "I am post2", user: users[1], comments_counter: 0, likes_counter: 0 },
+    { title: "post3", content: "I am post3", user: users[2], comments_counter: 0, likes_counter: 0 },
+    { title: "post4", content: "I am post4", user: users[3], comments_counter: 0, likes_counter: 0 },
+    { title: "post5", content: "I am post5", user: users[4], comments_counter: 0, likes_counter: 0 }
+])
+
+comments = Comment.create([
+    { content: "I am comment1", user: users[0], post: posts[0] },
+    { content: "I am comment2", user: users[1], post: posts[1] },
+    { content: "I am comment3", user: users[2], post: posts[2] },
+    { content: "I am comment4", user: users[3], post: posts[3] },
+    { content: "I am comment5", user: users[4], post: posts[4] }
+])
+
+likes = Like.create([
+    { user: users[0], post: posts[0] },
+    { user: users[1], post: posts[1] },
+    { user: users[2], post: posts[2] },
+    { user: users[3], post: posts[3] },
+    { user: users[4], post: posts[4] }
+])
+
+puts "Seeded #{User.count} users, #{Post.count} posts, #{Comment.count} comments, and #{Like.count} likes."
