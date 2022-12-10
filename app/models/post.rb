3972c319b7e311c7bc0b2f9afeefe294bc991922
ZikaZaki <zika.github@gmail.com>
2022-12-11 01:09:35 +0300

    Add validates to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   def self.update_post_counter(user, new_value)
     Post.find_by(user:).user.update(posts_counter: new_value)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true } 
+
   def retrieve_recent_posts
     posts.order(:created_at).limit(3)
   end
