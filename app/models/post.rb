b30ab8f051fe83506f9234ae4904fda5179a865f
ZikaZaki <zika.github@gmail.com>
2022-12-11 00:51:46 +0300

    Add update_post_counter & retrieve_recent_comments methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  def self.update_post_counter(user, new_value)
+    Post.find_by(user:).user.update(posts_counter: new_value)
+  end
+
+  def retrieve_recent_comments
+    comments.order(:created_at).limit(5)
+  end
 end
