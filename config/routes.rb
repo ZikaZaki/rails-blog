e94ceb4efb346cb26f8740e8a8105b3b17bcaa8f
ZikaZaki <zika.github@gmail.com>
2022-12-17 22:18:13 +0300

    Add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
   get "users/new", to: "users#new"
   post "users", to: "users#create"
   get "users/:id", to: "users#show"
   get "users/:id/edit", to: "users#edit"
-  root "users#index"
+  patch "users/:id", to: "users#update"
+  delete "users/:id", to: "users#destroy"
+  
+  get "users/:id/posts", to: "posts#index"
+  get "users/:id/posts/new", to: "posts#new"
+  post "users/:id/posts", to: "posts#create"
+  get "users/:id/posts/:post_id", to: "posts#show"
+  get "users/:id/posts/:post_id/edit", to: "posts#edit"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
