0dd529df7cbfb0398d5fa842a80d6e4a0a1939ed
ZikaZaki <zika.github@gmail.com>
2022-12-18 01:26:36 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Rails.application.routes.draw do
   patch "users/:id", to: "users#update"
   delete "users/:id", to: "users#destroy"
   
-  get "users/:id/posts", to: "posts#index"
-  get "users/:id/posts/new", to: "posts#new"
-  post "users/:id/posts", to: "posts#create"
+  get "users/:id/posts", to: "posts#index", as: 'user_posts'
+  get "users/:id/posts/new", to: "posts#new", as: 'new_post'
+  post "users/:id/posts/create", to: "posts#create", as: 'create_post'
   get "users/:id/posts/:post_id", to: "posts#show"
-  get "users/:id/posts/:post_id/edit", to: "posts#edit"
-  
+  get "users/:id/posts/:post_id/new_comment", to: "comments#new", as: 'new_comment'
+  get "users/:id/posts/:post_id/create", to: "comments#create", as: 'create_comment'
+  get "users/:id/posts/:post_id/likes", to: "likes#create", as: 'like_post'
   # Defines the root path route ("/")
   # root "articles#index"
 end
