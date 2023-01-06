1a1c398ccd2aef017812c34a22f777d292da25bb
ZikaZaki <zika.github@gmail.com>
2023-01-06 16:16:08 +0300

    Update project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     current_user.posts_counter += 1
     current_user.save
-    @post.author = current_user
+    @post.user_id = current_user.id
     @post.comments_counter = 0
     @post.likes_counter = 0
     if @post.save
@@ -31,50 +31,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # GET /posts/1/edit
-  def edit; end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     # params.require(:new_post).permit(:title, :content, :user_id)
     params.require(:new_post).permit(:title, :content)
-  end
-
-  def require_same_user
-    return unless current_user != @post.author
-
-    flash[:danger] = 'You can only edit or delete your own posts'
-    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,27 +24,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-    if @user.update(user_params)
-      flash[:success] = 'User was successfully updated'
-      redirect_to user_path(@user)
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @user.destroy
-    flash[:danger] = 'User was successfully deleted'
-    redirect_to users_path
-  end
-
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
   def user_params
     params.require(:user).permit(:name, :photo, :bio, :posts_counter)
   end
