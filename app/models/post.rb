9dd8d7d536d08f9f0b0aaafbad741b6933ed6840
ZikaZaki <zika.github@gmail.com>
2023-01-06 21:55:40 +0300

    Update project

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
   validates :title, presence: true, length: { minimum: 3, maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
